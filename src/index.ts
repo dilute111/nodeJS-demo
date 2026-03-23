e6e554bb850466a59c8450d1d7f66d2ccae5aa01
dilute111 <loshock666@gmail.com>
2026-03-23 14:37:10 +0500

    added CRUD operations for mocked products

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express, {type Request, type Response} from "express"
+import type {IAddress, IProduct} from "./types/index.js";
 
 const app = express()
 const port = process.env.PORT || 3000
 
-const products = [
+app.use(express.json())
+
+const products: IProduct[] = [
     {id: 1, title: "tomato"},
     {id: 2, title: "orange"},
 ]
-const addresses = [
+const addresses: IAddress[] = [
     {id: 1, value: "Nezalejnasti 12"},
     {id: 2, value: "Selickaga 11"},
 ]
-
+// Получить данные
 app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello World!.EU!';
     res.send(helloMessage)
@@ -54,8 +57,35 @@ app.get('/addresses/:id', (req: Request, res: Response) => {
     }
     res.json(queryAddress)
 })
-
+// Удалить данные
 app.delete('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+    if (!id) {
+        res.sendStatus(400)
+        return
+    } else {
+        for (let i = 0; i < products.length; i++) {
+            const product = products[i]
+            if (product && product.id === +id) {
+                products.splice(i, 1)
+                res.sendStatus(204)
+                return
+            }
+        }
+    }
+    res.sendStatus(404)
+})
+// Установить данные
+app.post('/products', (req: Request, res: Response) => {
+    const newProduct: IProduct = {
+        id: products.length + 1,
+        title: req.body.title
+    }
+    products.push(newProduct)
+    res.status(201).json(newProduct)
+})
+// Обновить данные
+app.put('/products/:id', (req: Request, res: Response) => {
     const id = req.params.id
     if (!id) {
         res.sendStatus(400)
@@ -65,8 +95,10 @@ app.delete('/products/:id', (req: Request, res: Response) => {
     if (!queryProduct) {
         res.sendStatus(404)
         return
+    } else {
+        queryProduct.title = req.body.title
+        res.status(201).json(queryProduct)
     }
-    res.json(queryProduct)
 })
 
 app.listen(port, () => {
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+    id: number
+    title: string
+}
+export interface IAddress {
+    id: number
+    value: string
+}
